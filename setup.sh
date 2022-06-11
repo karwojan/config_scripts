3c563f3ea76c38aab9ab9b9e148739e6bde39a04
Jan Karwowski <karwojan@tlen.pl>
2022-06-11 12:56:56 +0200

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,11 @@
+mkdir -p ~/.vim/ftplugin
+mkdir -p ~/.vim/autoload
+mkdir -p ~/.config/i3
+mkdir -p ~/.config/i3status
+
+curl -fLo ~/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+ln -s `pwd`/i3_config ~/.config/i3/config
+ln -s `pwd`/i3status_config ~/.config/i3_status/config
+ln -s `pwd`/Xresources ~/.Xresources 
+cd ./vim
+find ./ -type f -exec ln -s `pwd`/{} ~/.vim/{}  \;
